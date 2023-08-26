4696a86cfa1a211911c67b824af299c772d52db6
vorue <vorue@infoil.com.ar>
2023-08-26 10:37:23 -0300

    error test

--- app/src/test/java/com/gruoptwo/proyectofinal/ExampleUnitTest.kt
+++ app/src/test/java/com/gruoptwo/proyectofinal/ExampleUnitTest.kt
@@ -14,4 +14,10 @@ class ExampleUnitTest {
     fun addition_isCorrect() {
         assertEquals(4, 2 + 2)
     }
+
+    @Test
+    fun addition_isnotCorrect() {
+        assertEquals(5, 2 + 2)
+    }
+
 }
